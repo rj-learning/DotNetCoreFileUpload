b440b74ed25f79456798894afc33eda5b032d3b7
Rohit Jadhav <rohitjadhav.dev@gmail.com>
2023-01-01 09:42:22 +0530

    Add project files.

---
+++ TestApp/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TestApp.Models;
+using static System.Net.WebRequestMethods;
+
+namespace TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _environment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(MyForm myForm)
+        {
+            var files = myForm.Files;
+            foreach (IFormFile source in files)
+            {
+                string filename = ContentDispositionHeaderValue.Parse(source.ContentDisposition).FileName.Trim('"');
+
+                filename = this.EnsureCorrectFilename(filename);
+                string fileWithPath = this.GetPathAndFilename(filename);
+                Directory.CreateDirectory(Path.GetDirectoryName(fileWithPath));
+
+                using (FileStream output = System.IO.File.Create(fileWithPath))
+                    await source.CopyToAsync(output);
+            }
+
+            return Ok("Success");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public class MyForm
+        {
+            public string First { get; set; }
+            public string Middle { get; set; }
+            public IList<IFormFile> Files { get; set; }
+        }
+
+        private string EnsureCorrectFilename(string filename)
+        {
+            if (filename.Contains("\\"))
+                filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+
+            return filename;
+        }
+
+        private string GetPathAndFilename(string filename)
+        {
+            return Path.Combine(_environment.ContentRootPath, "uploadedFiles", filename);
+        }
+    }
+}
